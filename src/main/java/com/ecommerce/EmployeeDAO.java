689c7ffea250cf4af90bf66e9f0b333b19d7a4fa
shrutik15git <tishrupal@gmail.com>
2022-02-17 15:17:35 -0500

    first commit

---
+++ src/main/java/com/ecommerce/EmployeeDAO.java
@@ -0,0 +1,29 @@
+package com.ecommerce;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.Employee;
+import com.ecommerce.Employees;
+ 
+@Repository
+
+public class EmployeeDAO {
+	private static Employees list = new Employees();
+    
+    static
+    {
+        list.getEmployeeList().add(new Employee(1, "Alex", "Kolenchiskey", "abc@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "David", "Kameron", "titanic@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "Sam", "Kameron", "sam@gmail.com"));
+        list.getEmployeeList().add(new Employee(4, "John", "Smith", "john@gmail.com"));
+        list.getEmployeeList().add(new Employee(5, "William", "Smith", "william@gmail.com"));
+    }
+     
+    public Employees getAllEmployees() 
+    {
+        return list;
+    }
+     
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
